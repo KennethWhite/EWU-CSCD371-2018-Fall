dafaf6f419c1b9a893dc31dd6bb4d24f48a57a1c
Kenneth White <kennival@hotmail.com>
2018-09-24 19:04:07 -0700

    Added basic program, improved .gitignore

---
+++ Assignment1/EchoProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class EchoProgram
+    {
+        static void Main(string[] args)
+        {
+            String userPhrase;
+
+            Console.Write("Enter a phrase for the console to echo back: ");
+            userPhrase = Console.ReadLine();
+            Console.WriteLine($"Echo: {userPhrase}");
+        }
+    }
+}
