b9dca5d497ee8ac3c11080af130538968cb252ea
Kenneth White <kennival@hotmail.com>
2018-10-08 20:12:20 -0700

    Created project

---
+++ Assignment3/Assignment3/Assignment3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CREATE TUPLE PLAYER COMPUTER
+            //Welcome to RPS
+            //Rules (dynamic nums)
+            bool runFlag = true;
+            while (runFlag)
+            {
+                //prompt
+                //GetUsersChoice
+                //CheckUsersChoice
+                //GetComputerChoice
+                //EvaluateRound (tuple)
+                //CalculateHealthPoints
+                if (GameIsOver)
+                {
+                    //runFlag = continuePrompt()
+                }
+            }
+        }
+    }
