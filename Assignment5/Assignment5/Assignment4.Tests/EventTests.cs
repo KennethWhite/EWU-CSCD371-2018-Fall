5a592fd5280973c6e64aa0f93e1dabe7afdcd0b5
Kenneth White <kennival@hotmail.com>
2018-10-28 09:38:17 -0700

    Created solution including Assignment 4 classes

---
+++ Assignment5/Assignment5/Assignment4.Tests/EventTests.cs
@@ -0,0 +1,110 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Event.Tests
+{
+    [TestClass]
+    public class EventTests
+    {
+
+        private static Assignment4.Event Event { get; set; }
+
+        [TestInitialize]
+        public void SetupUnivCourse()
+        {
+            Event = new Assignment4.Event(
+                ".Net GUI", "CEB 207", new DateTime(2018, 10, 14, 14, 0, 0), new DateTime(2018, 10, 14, 16, 30, 0));
+        }
+
+        [TestMethod]
+        public void Event_TestConstructor_ObjectCreated()
+        {
+            Assignment4.Event e = new Assignment4.Event("TEST", "TEST", new DateTime(2018, 10, 14, 14, 0, 0), new DateTime(2018, 10, 14, 16, 30, 0));
+        }
+
+        [TestMethod]
+        public void GetTitle_ReturnsValue()
+        {
+            Assert.AreEqual(".Net GUI", Event.Title);
+        }
+
+        [TestMethod]
+        public void SetTitle_TitleUpdated()
+        {
+            Event.Title = "TEST_VALUE";
+            Assert.AreEqual("TEST_VALUE", Event.Title);
+        }
+
+        [TestMethod]
+        [DataRow("")]
+        [DataRow(" ")]
+        [DataRow("        ")]
+        [DataRow("S")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SetTitle_InvalidData_ThrowsException(string test)
+        {
+            Event.Title = test;
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetLocation_ReturnsValue()
+        {
+            Assert.AreEqual("CEB 207", Event.Location);
+        }
+
+        [TestMethod]
+        public void SetLocation_LocationUpdated()
+        {
+            Event.Location = "TEST_VALUE";
+            Assert.AreEqual("TEST_VALUE", Event.Location);
+        }
+
+        [TestMethod]
+        public void GetStartDate_ReturnsValue()
+        {
+            Assert.AreEqual(new DateTime(2018, 10, 14, 14, 0, 0), Event.StartDate);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SetDate_EndDateBeforeStartDate_ThrowsException()
+        {
+            Assignment4.Event e = new Assignment4.Event(
+                ".Net GUI", "CEB 207",
+                new DateTime(2018, 10, 14, 14, 0, 0),
+                new DateTime(2018, 10, 14, 10, 30, 0));
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetEventsCreated_ReturnsIncrementedValue()
+        {
+            int count = Assignment4.Event.EventsCreated;
+            Assignment4.Event e = new Assignment4.Event(".Net GUI", "CEB 207", 
+                new DateTime(2018, 10, 14, 14, 0, 0), new DateTime(2018, 10, 14, 16, 30, 0));
+            Assert.AreEqual(count+1, Assignment4.Event.EventsCreated);
+        }
+
+        [TestMethod]
+        public void GetEventsCreated_TestingDerivedClass_ReturnsIncrementedValue()
+        {
+            int count = Assignment4.Event.EventsCreated;
+            Assignment4.Event e = new Assignment4.UniversityCourse(
+                ".Net GUI", "CEB 207", new DateTime(2018, 10, 14, 14, 0, 0), new DateTime(2018, 10, 14, 16, 30, 0),
+                "CSCD", "371", "Michaelis", "T Th");
+            Assert.AreEqual(count + 1, Assignment4.Event.EventsCreated);
+        }
+
+        [TestMethod]
+        public void GetSummaryInfo_ReturnsSummary()
+        {
+            Assert.AreEqual(
+                $"Title: {Event.Title}\n" +
+                $"Location: {Event.Location}\n" +
+                $"Dates: {Event.StartDate}-{Event.EndDate}",
+                Event.GetSummaryInformation());
+
+        }
+    }
+}
---
+++ Assignment5/Assignment5/Assignment4/UniversityCourse.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Assignment4
+{
+    public class UniversityCourse : Event
+    {
+
+
+        public UniversityCourse(string title, string location, DateTime start,
+            DateTime end,string dep, string id, string instructor, string daysOfWeek) :
+            base(title, location, start, end)
+        {
+            Department = dep;
+            CourseID = id;
+            Instructor = instructor;
+            Schedule = daysOfWeek;
+            CoursesCreated++;
+        }
+
+        public void Deconstruct(out string title, out string location,
+            out string instructor, out DateTime start, out DateTime end)
+        {
+            (title, location, instructor, start, end) =
+                (Title, Location, Instructor, StartDate, EndDate);
+        }
+
+        public override string Title
+        {
+            get
+            {
+                return $"{Department} {CourseID}";
+            }
+
+        }
+
+        public string Department
+        {
+            get => _Department;
+            set
+            {
+
+                if (value.Length < 3 || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException($"Department code must be at least 3 characters {value}");
+                }
+                _Department = value;
+
+            }
+        }
+        private string _Department;
+
+        public string CourseID
+        {
+            get => _CourseID;
+            set
+            {
+                if (value.Length < 3 || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException($"CourseID number must be at least 3 characters {value}");
+                }
+                _CourseID = value;
+            }
+
+        }
+        private string _CourseID;
+
+        public string Instructor
+        {
+            get; set;
+        }
+        private string _Instructor;
+
+        public string Schedule
+        {
+            get => _Schedule;
+            set
+            {
+                if (value.Length < 1 || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException($"Schedule must be at least 1 day per week: {value}");
+                }
+                _Schedule = value;
+            }
+        }
+        private string _Schedule;
+
+        public double Credits
+        {
+            get
+            {
+                string[] days = Schedule.Split(" ");
+                double hoursPerDay = EndDate.Hour - StartDate.Hour + (EndDate.Minute - StartDate.Minute) / 60.0;
+                return days.Length * hoursPerDay;
+            }
+        }
+
+        public static int CoursesCreated
+        {
+            get;
+            private set;
+        }
+        private static int _CoursesCreated;
+
+        public override string GetSummaryInformation()
+        {
+            return base.GetSummaryInformation() +
+                $"Instructor: {Instructor}\n" +
+                $"Schedule: {Schedule}";
+
+        }
+
+
+
+    }
+}
