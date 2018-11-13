287a893b46a1c237797349c65c16f67fcef81c52
Kenneth White <kennival@hotmail.com>
2018-11-12 17:08:50 -0800

    Created empty classes required for this project

---
+++ Assignment8/Assignment8.Tests/IDateTime.Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment8.Tests
+{
+    [TestClass]
+    public class IDateTimeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Assignment8/Assignment8.Tests/TimeManager.Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment8.Tests
+{
+    [TestClass]
+    public class TimeManagerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
