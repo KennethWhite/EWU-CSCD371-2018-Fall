fc6cf9bcdc3ac4b212b292915b2a57fe04689be3
Kenneth White <kennival@hotmail.com>
2018-11-13 15:29:54 -0800

    Small additions to TimeManager demnstrating some declarations from class

--- Assignment8/TimeManager/TimeManager.cs
+++ Assignment8/TimeManager/TimeManager.cs
@@ -8,5 +8,27 @@ namespace TimeManager
 {
     public class TimeManager
     {
+        public event Func<int> GetValue;
+        public event Action TimeEventCreated;
+        public event EventHandler<EventArgs> GenericEvent;
+        public event EventHandler<AddEventArgs> AddEvent;
+
+        public void RaiseEvent()
+        {
+            TimeEventCreated?.Invoke();
+        }
+
+        public int RaiseGetValue() => GetValue?.Invoke() ?? 0;
+
+        public void RaiseGenericEvent(EventArgs e)
+        {
+
+        }
+
+        public class AddEventArgs : EventArgs
+        {
+
+        }
+
     }
 }
