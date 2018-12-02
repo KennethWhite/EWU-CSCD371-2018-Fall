28a857ed93d3cb7e63d8ee6f0613b1395c748c31
Kenneth White <kennival@hotmail.com>
2018-12-01 20:15:49 -0800

    Reworked solution based upon feedback given in class. Corrected some logical errors in the unit tests

--- Assignment9/PatentDataAnalyzer/Assignment9Tests/PatentDataAnalyzer.Tests.cs
+++ Assignment9/PatentDataAnalyzer/Assignment9Tests/PatentDataAnalyzer.Tests.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Linq;
 using PatentDataAnalyzer;
+using System;
 
 namespace Assignment9Tests
 {
@@ -36,21 +37,21 @@ namespace Assignment9Tests
                 "Jacob"
             };
             IEnumerable<string> lastNames = PatentDataAnalyzer.PatentDataAnalyzer.InventorLastNames();
-            foreach(string name in lastNames)
+            foreach (string name in lastNames)
             {
                 Assert.IsTrue(validLastNames.Contains(name), $"Could not find {name} in valid names.");
             }
-            
+
         }
 
         [TestMethod]
         public void InventorLastNames_ReturnsNamesInReverseOrderById()
         {
             IEnumerable<string> lastNames = PatentDataAnalyzer.PatentDataAnalyzer.InventorLastNames();
-            int index = PatentData.Inventors.Length-1;
+            int index = PatentData.Inventors.Length - 1;
             var expectedOrder = PatentData.Inventors.OrderByDescending(x => x.Id);
             //sequence equals
-            foreach(string name in lastNames)
+            foreach (string name in lastNames)
             {
                 Assert.IsTrue(PatentData.Inventors[index].Name.EndsWith(name), "");
                 index--;
@@ -61,13 +62,14 @@ namespace Assignment9Tests
         public void LocationsWithInventors_ReturnsDistinctList()
         {
             string result = PatentDataAnalyzer.PatentDataAnalyzer.LocationsWithInventors();
-            string[] locations = result.Split(", ");
-            //List<string> locationsList = new List<string>(locations);
-            //this doesnt make sense logically
-            foreach(string location in locationsList)
+            string[] resultLocations = result.Split(", ");
+            Inventor[] inventors = PatentData.Inventors;
+            List<string> locationsList = new List<string>(inventors.Select(inventor => $"{inventor.State}-{inventor.Country}"));
+            foreach (string location in locationsList)
             {
                 Assert.IsTrue(
-                    locationsList.Where(loc => loc != null && loc.Equals(location)).Count() == 1);
+                    resultLocations.Where(loc => loc != null && loc.Equals(location)).Count() == 1,
+                    $"Failed : {location}");
             }
         }
 
@@ -75,7 +77,7 @@ namespace Assignment9Tests
         public void Randomize_ReturnsNewOrderThreeConsecutiveTimes_Success()
         {
             IEnumerable<Inventor> prevList = new List<Inventor>();
-            for (int iteration = 0; iteration < 3; iteration++ )
+            for (int iteration = 0; iteration < 3; iteration++)
             {
                 IEnumerable<Inventor> randomizedList =
                 PatentDataAnalyzer.Enumerable.Randomize<Inventor>(PatentData.Inventors);
@@ -87,20 +89,35 @@ namespace Assignment9Tests
         }
 
         [TestMethod]
-        public void NthFibonacciNumbers_ReturnsFibonacciSequenceToN()
+        [DataRow(1,new int[] {1, 1, 2, 3} )]
+        [DataRow(2,new int[] {1, 3, 8, 21} )]
+        [DataRow(3,new int[] {2, 8, 34, 144} )]
+        public void NthFibonacciNumbers_ReturnsFibonacciSequenceToN(int n, int[] expected)
         {
-            Assert.Fail();
+     
             List<int> fibs = new List<int>
             {
                 1,1,2,3,5,8,13,21,34,55,89,144
             };
 
-            int[] results = PatentDataAnalyzer.PatentDataAnalyzer.NthFibbonacciNumbers(fibs.Count).ToArray();
-            for (int n = 0; n < fibs.Count; n++)
-            {
-                Assert.AreEqual(fibs[n], results[n]);
-            }
+            List<int> results = 
+                PatentDataAnalyzer.PatentDataAnalyzer.NthFibonacciNumbers(n).Take(expected.Length).ToList();
+            Assert.IsTrue(results.SequenceEqual(expected),
+                $"Expected : {expected}{Environment.NewLine}" +
+                $"Actual : {results}");
         }
+
+        [TestMethod]
+        [DataRow(1, 6)]
+        [DataRow(2, 0)]
+        [DataRow(3, 1)]
+        public void GetInventorsWithMultiplePatents_ResultsHaveMultiplePatents(int numOfPatents, int expectedNumberOfResults)
+        {
+            //Not an elegant way to test this, relies on the dataset not changing.
+            List<Inventor> results = PatentDataAnalyzer.PatentDataAnalyzer.GetInventorsWithMultiplePatents(numOfPatents);
+            Assert.AreEqual(expectedNumberOfResults, results.Count);
+        }
+
 
     }
 }
